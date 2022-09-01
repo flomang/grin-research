7dfbecec8b7d4daa6d08a1d7e206e3fbf77510c1
flo <inbox.733@gmail.com>
2022-09-01 12:37:26 -0600

    Cleanup.

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,15 +1,14 @@
 use clap::Parser;
 use easy_jsonrpc_mw::{BoundMethod, Response};
-use futures::executor::block_on;
 use grin_api::foreign_rpc::foreign_rpc;
-use grin_pool::types::PoolEntry;
+//use grin_pool::types::PoolEntry;
 use log::info;
 use reqwest::Client;
 use serde::Deserialize;
 use serde_json::Value;
 use std::net::SocketAddr;
 use std::sync::mpsc;
-use std::time::Duration;
+//use std::time::Duration;
 use std::{thread, time};
 
 #[derive(Clone, Parser, Debug)]
@@ -54,52 +53,53 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         info!("grin api: {:?}", grin_version);
         tx1.send(grin_version).unwrap();
 
+        let mut current_height = 0;
         let delay = time::Duration::from_secs(1);
-        let mut all_txns: Vec<PoolEntry> = vec![];
-
-        let grin_tip = rpc(&grin_addr, &foreign_rpc::get_tip().unwrap())
-            .unwrap()
-            .unwrap();
-        let mut current_height = grin_tip.height;
-        info!("height: {:?}", current_height);
+        //let mut all_txns: Vec<PoolEntry> = vec![];
 
         loop {
-            if let Ok(txns) = rpc(
-                &grin_addr,
-                &foreign_rpc::get_unconfirmed_transactions().unwrap(),
-            )
-            .unwrap()
-            {
-                let grin_tip = rpc(&grin_addr, &foreign_rpc::get_tip().unwrap()).unwrap().unwrap();
+            let grin_tip = rpc(&grin_addr, &foreign_rpc::get_tip().unwrap())
+                .unwrap()
+                .unwrap();
 
-                if current_height < grin_tip.height {
-                    current_height = grin_tip.height;
-                    let block = rpc(
-                        &grin_addr,
-                        &foreign_rpc::get_block(Some(current_height), None, None).unwrap(),
-                    )
-                    .unwrap().unwrap();
-                    info!("Supply: {}", block.header.height * 60 + 60);
-                    info!("new block: {}\n{:#?}", block.header.height, block);
-                }
+            if current_height < grin_tip.height {
+                current_height = grin_tip.height;
 
-                if all_txns.len() != txns.len() {
-                    all_txns = txns;
-                    for txn in all_txns.iter() {
-                        let inputs = txn.tx.body.inputs.len();
-                        let outputs = txn.tx.body.outputs.len();
-                        let kernels = txn.tx.body.kernels.len();
+                let block = rpc(
+                    &grin_addr,
+                    &foreign_rpc::get_block(Some(current_height), None, None).unwrap(),
+                )
+                .unwrap()
+                .unwrap();
 
-                        info!("----");
-                        info!("\t at: {}", txn.tx_at);
-                        info!("\t src: {:?}", txn.src);
-                        info!("\t kernels: {:?}", kernels);
-                        info!("\t inputs: {:?}", inputs);
-                        info!("\t outputs: {:?}", outputs);
-                        info!("\t tx: {:?}", txn.tx);
-                    }
-                }
+                info!("supply: {}", block.header.height * 60 + 60);
+                info!("block tip at: {}", block.header.height);
             }
+
+            // if let Ok(txns) = rpc(
+            //     &grin_addr,
+            //     &foreign_rpc::get_unconfirmed_transactions().unwrap(),
+            // )
+            // .unwrap()
+            // {
+
+            //     if all_txns.len() != txns.len() {
+            //         all_txns = txns;
+            //         for txn in all_txns.iter() {
+            //             let inputs = txn.tx.body.inputs.len();
+            //             let outputs = txn.tx.body.outputs.len();
+            //             let kernels = txn.tx.body.kernels.len();
+
+            //             info!("----");
+            //             info!("\t at: {}", txn.tx_at);
+            //             info!("\t src: {:?}", txn.src);
+            //             info!("\t kernels: {:?}", kernels);
+            //             info!("\t inputs: {:?}", inputs);
+            //             info!("\t outputs: {:?}", outputs);
+            //             info!("\t tx: {:?}", txn.tx);
+            //         }
+            //     }
+            // }
             thread::sleep(delay);
         }
     });
